d2736faa448ce36c8b61229a87b0f918ed376b02
Michael Cardona <mcardona9015@gmail.com>
2020-11-22 21:15:30 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  count = 0
+  max_val = 0
+  while count < array.length do |index|
+    if index > max_val
+      max_val = index
+    end
+    count += 1
+  end
+  max_val
 end
